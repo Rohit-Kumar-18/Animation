fc801dc62977930f2a381fbb4302568d4dd57641
Rohit-Kumar-18 <rohitk73423@gmail.com>
2025-08-21 01:27:53 +0530

    Updated animated project files

--- PROJECTS/animated.js
+++ PROJECTS/animated.js
@@ -54,3 +54,32 @@ function navAnimation() {
         }
 
         navAnimation();
+
+
+function page2Animation() {
+    var rightElems=document.querySelectorAll(".right-elem")
+        rightElems.forEach(function(elem){
+           elem.addEventListener("mouseenter",function(){
+
+            //  console.log(elem.getBoundingClientRect())
+
+            gsap.to(elem.childNodes[3],{
+                opacity:1,
+                scale:1
+            })
+           })
+           elem.addEventListener("mouseleave",function(){
+             gsap.to(elem.childNodes[3],{
+                opacity:0,
+                scale:0
+             })
+           })
+           elem.addEventListener("mousemove",function(dets){
+            gsap.to(elem.childNodes[3],{
+                x:dets.x - elem.getBoundingClientRect().x-20,
+                y:dets.y - elem.getBoundingClientRect().y-100
+            })
+           })
+        })
+}
+page2Animation()
